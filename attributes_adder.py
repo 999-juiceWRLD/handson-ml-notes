47fcfaaf2efd6fb166ee2548cc948160e9f591e6
juice wrld <ankaraunifedaisi@gmail.com>
2022-12-03 14:09:28 +0300

    initial commit

---
+++ attributes_adder.py
@@ -0,0 +1,25 @@
+from sklearn.base import BaseEstimator, TransformerMixin
+from fetch_data import load_housing_data
+import pandas as pd
+import numpy as np
+
+housing = load_housing_data()
+
+rooms_ix, bedrooms_ix, population_ix, households_ix = 3, 4, 5, 6
+
+class CombinedAttributesAdder(BaseEstimator, TransformerMixin):
+    def __init__(self, add_bedrooms_per_room=True):
+        self.add_bedrooms_per_room = add_bedrooms_per_room
+    
+    def fit(self, X, y=None):
+        return self # yapacak bir şey yok
+    
+    def transform(self, X, y=None):
+        rooms_per_household = X[:, rooms_ix] / X[:, households_ix]
+        population_per_household = X[:, population_ix] / X[:, households_ix]
+        if self.add_bedrooms_per_room:
+            bedrooms_per_room = X[:, bedrooms_ix] / X[:, rooms_ix]
+            return np.c_[X, rooms_per_household, population_per_household, bedrooms_per_room]
+        else:
+            return np.c_[X, rooms_per_household, population_per_household]
+        
